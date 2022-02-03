4e6a8d541f31e9d257c4639c66cad5e95633b987
student <student@ip-172-31-55-70.us-west-2.compute.internal>
2022-02-02 19:56:30 -0800

    Secret Message

--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -19,14 +19,16 @@ public class SecretMessageBox {
 	// 0. Make a main method and put steps 1-5 inside it
 	
 	// 1. Set a password in a String variable
-	
+	secretPassword = 1234;
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	 JOptionPane.showMessageDialog(null,"What is the secret password?");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+	 JOptionPane.showInputDialog(null,"What is the password?");
 	// 4. If their guess matches the password, show them the secret message
-
+if (secretPassword= 1234){
+	JOptionPane.showMessageDialog(null, "You suck :D");
+	}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
 }
